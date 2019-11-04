542ca40c425a579f2fbde435b84efaf324895aa9
Андрей Тарарин <andrei.t.web@gmail.com>
2019-11-04 19:18:07 +0400

    Version 1.0 complete

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,35 +1,107 @@
 import React from 'react';
 import './App.css';
 
+import moment from 'moment/min/moment.min';
+
 // Components
 import Form from "./components/Form";
 
 const APIKey = '7b9cd48c135743519b7158ef910b185d';
-const APIPath = 'http://api.weatherbit.io/v2.0/';
+const APIPath = 'http://api.weatherbit.io/v2.0/forecast/daily';
+const API_ICON_PATH = 'https://www.weatherbit.io/static/img/icons/';
 
 class WeatherApp extends React.PureComponent{
   constructor(props){
     super(props);
 
     this.state = {
-      lang: 'ru',
-      units: 'M', // Metrics (Celcius)
-      days: '16', // Forecast days
-      city: '',
-      country: '',
       data: [],
+      city: '',
+      error: '',
     }
   }
 
+  getString = (city, days) => {
+    return `${APIPath}${city ? `?city=${city}` : ''}${days ? `&days=${days}` : ''}&key=${APIKey}`;
+  };
+
+  onSubmit = (city, days) => {
+    const url = this.getString(city, days);
+    fetch(url)
+        .then(res=>res.json())
+        .then(res=>{
+          this.setState({
+              data: res.data,
+              city: res['city_name'],
+              error: ''
+          });
+        })
+        .catch(error=>{
+            this.setState({
+                error: 'Wrong city name. Please try enter city name again.'
+            })
+        })
+  };
+
+  get date(){
+      return this.state.data.map(day=>moment(day.datetime).format('dddd'));
+  }
+
   render() {
+    const {data, city, error} = this.state;
+    const date = this.date;
     return  (
-      <div className='weather'>
-        <div className='weather__header'>
-          <div className='weather__form'>
-            <Form/>
-          </div>
+        <div className='weather'>
+            <div className='weather__bg'>
+                <div className='weather__img' style={{backgroundImage: 'url("images/cloudy.jpg")'}}>
+                </div>
+            </div>
+            <div className='weather__form'>
+                <Form onSubmit={this.onSubmit}/>
+            </div>
+            <div className='weather__content'>
+                {error ?
+                    <div className='alert alert-warning'>{error}</div> :
+                    <div className='weather__list'>
+                        {data.map((day, index)=>
+                            <div key={index} className='weather__item'>
+                                <div className='card weather__day weather__day_front'/>
+                                <div className='card weather__day weather__day_back'>
+                                    <div className='weather__day-header card-header'>
+                                        <span className='font-weight-bold'>{date[index]}</span>
+                                        <span className='weather__city'>{city}</span>
+                                    </div>
+                                    <div className='weather__day-body card-body'>
+                                        <div className='weather__icon'>
+                                            <img src={`${API_ICON_PATH}${day.weather.icon}.png`} alt={day.weather.description}/>
+                                        </div>
+                                        <div className='weather__day-data'>
+                                            <div className='weather__temp'>
+                                                <span>{Math.round(day['max_temp'])}&deg;</span>
+                                                <span> / </span>
+                                                <span>{Math.round(day['min_temp'])}&deg;</span>
+                                            </div>
+                                            <div className='weather__wind'>
+                                                <img src="images/icons/wind.png" alt="Wind"/>
+                                                <span>{Math.round(day['wind_spd'])} m/s</span>
+                                            </div>
+                                        </div>
+                                        <div className='weather__day-data'>
+                                            <div className='weather__temp weather__temp_feel'>
+                                                <span>Feels Like: </span>
+                                                <span>{Math.round(day['app_max_temp'])}&deg;</span>
+                                                <span> / </span>
+                                                <span>{Math.round(day['app_min_temp'])}&deg;</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                }
+            </div>
         </div>
-      </div>
     );
   }
 }
--- weather-app/src/components/Form/index.js
+++ weather-app/src/components/Form/index.js
@@ -1,85 +1,40 @@
 import React from 'react';
-import debounce from 'lodash/debounce';
+import PropTypes from 'prop-types';
 
-import './styles.css';
+import './styles.css'
 
-const DADATA_Key = '1e20133e0ad033c1fa13940edaa51f56552366a6';
-const DADATA_URL = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address';
-const MIN_VALUE_LENGTH = 2;
-const MAX_COUNT_DATA = 6;
+const MAX_DAYS = 16;
 
-class Form extends React.PureComponent{
-    constructor(props) {
-        super(props);
+function Form(props) {
 
-        this.state = {
-            data: [],
-            allCountries: false,
-        };
-
-        this.loadDataDebounde = debounce(this.loadData, 500);
-    }
-
-    inputHandler = (event)=>{
-        const value = event.target.value;
-        value.length >= MIN_VALUE_LENGTH && this.loadDataDebounde(value);
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        const city = event.target.elements['city_name'].value;
+        const days = event.target.elements['count_days'].value;
+        props.onSubmit(city, days);
     };
 
-    loadData = (value) => {
-        fetch(DADATA_URL, {
-            'method': 'POST',
-            'headers': {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Token ${DADATA_Key}`
-            },
-            body: JSON.stringify({
-                'query': value,
-                'count': MAX_COUNT_DATA,
-                'from_bound': { 'value': 'city' },
-                'to_bound': { 'value': 'city' },
-                'locations': [
-                    {
-                        'country': this.state.allCountries ? '*' : 'Россия',
-                    }
-                ]
-            })
-        })
-        .then(response=>response.json())
-        .then(response => {
-            console.log(response);
-            this.setState({
-                data: response.suggestions,
-            })
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    };
-
-    get cities() {
-        return this.state.data.map((city)=>city.value);
-    }
-
-    render() {
-        return (
-            <div>
-                <div className='form-group'>
-                    <label htmlFor='city_name'>Введите город:</label>
-                    <input
-                        type='text'
-                        name='city_name'
-                        className='form-control'
-                        onChange={this.inputHandler}/>
-                </div>
-                <div>
-                    {this.cities.map((city, index)=>{
-                        return <div className='item' key={index}>{city}</div>
-                    })}
-                </div>
+    return (
+        <form className='weather-form form' onSubmit={onSubmitHandler}>
+            <div className='form-group'>
+                <label htmlFor="city_name">Enter city name:</label>
+                <input type="text" name='city_name' id='city_name' className='form-control'/>
             </div>
-        );
-    }
+            <div className='form-group'>
+                <label htmlFor="count_days">Enter count days:</label>
+                <select name='count_days' id='count_days' className='custom-select' defaultValue={MAX_DAYS}>
+                    {[...new Array(MAX_DAYS)].map((day, index)=>(
+                        <option value={index+1} key={index+1}>{index+1}</option>
+                    ))}
+                </select>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    )
 }
+
+Form.propTypes = {
+    onSubmit: PropTypes.func,
+};
 
 export default Form;
